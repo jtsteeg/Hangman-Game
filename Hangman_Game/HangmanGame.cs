2ab9fcacd5a89130674c1e634a03564fd9674597
Jacob Steeg <jacob.t.steeg@gmail.com>
2021-09-12 13:01:19 -0500

    adds introduction and main gameplay loop

---
+++ Hangman_Game/HangmanGame.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman_Game
+{
+    class HangmanGame
+    {
+        bool gameOver = false;
+
+        public HangmanGame() 
+        { 
+            while(gameOver == false)
+                {
+                Console.WriteLine("type 'exit' to quit");
+                string userCommand = Console.ReadLine().ToLower();
+                switch (userCommand)
+                {
+                    case "exit":
+                        Console.WriteLine("goodbye");
+                        gameOver = true;
+                        break;
+                    default:
+                        break;
+
+                }
+            }
+        }
+    }
+}
--- Hangman_Game/Program.cs
+++ Hangman_Game/Program.cs
@@ -6,7 +6,8 @@ namespace Hangman_Game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Hangman Game!");
+            new HangmanGame();
         }
     }
 }
