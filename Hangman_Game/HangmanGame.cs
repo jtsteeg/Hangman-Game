ea2292435f2097d88df3ce8eabae1ab3539834a5
Jacob Steeg <jacob.t.steeg@gmail.com>
2021-09-12 13:34:20 -0500

    creates 'WordToList' class to retrieve random word as char list

--- Hangman_Game/HangmanGame.cs
+++ Hangman_Game/HangmanGame.cs
@@ -9,12 +9,17 @@ namespace Hangman_Game
     class HangmanGame
     {
         bool gameOver = false;
+        int turnsLeft = 10;
+        List<char> mysteryList = new List<char>();
+        List<char> guessList = new List<char>();
 
         public HangmanGame() 
-        { 
+        {
+            var d = new WordToList();
+            mysteryList = d.MakeList();
             while(gameOver == false)
                 {
-                Console.WriteLine("type 'exit' to quit");
+                Console.WriteLine("type '*' to quit");
                 string userCommand = Console.ReadLine().ToLower();
                 switch (userCommand)
                 {
---
+++ Hangman_Game/WordToList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman_Game
+{
+    class WordToList
+    {
+        private List<string> wordLibrary;
+        public WordToList()
+        {
+            List<string> wordLibrary = new List<string>() { "table", "chair", "lamp", "desk", "chandelier" };
+        }
+
+
+        private string GetRandomWord()
+        {
+            var random = new Random();
+            var i = random.Next(0, wordLibrary.Count - 1);
+            return wordLibrary[i];
+        }
+
+        public List<char> MakeList()
+        {
+            string mysteryWord = GetRandomWord();
+            List<char> mysteryList = new List<char>();
+
+            foreach (var item in mysteryWord)
+            {
+                mysteryList.Add(item);
+            }
+
+            return mysteryList;
+        }
+    }
+}
